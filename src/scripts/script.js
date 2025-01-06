57d298067cb3c99ebcdccd84ea95cf36364e55e7
Asolvi-TManders <tim.manders@asolvi.com>
2025-01-06 12:24:47 +0000

    Copied in code from offline version

---
+++ src/scripts/script.js
@@ -0,0 +1,65 @@
+const defaultFrenchWordsArray = ["Bonjour", "Au revoir", "J'adore"];
+const defaultEnglishTranslationArray = ["Hello", "Goodbye", "I love"];
+
+let actionButton = document.getElementById('next_action');
+let translateButton = document.getElementById('translate');
+let currentWord = document.getElementById('current_word');
+let cardTitle = document.getElementById('card_title');
+let addToList = document.getElementById('add-words');
+let newFrenchWord = document.getElementById('new-french-word');
+let newEnglishWord = document.getElementById('new-english-word');
+let resetList = document.getElementById('reset-list');
+let counter = 0;
+
+let frenchWordsArray = JSON.parse(localStorage.getItem("frenchWords"))
+if (frenchWordsArray === null) {
+    frenchWordsArray = defaultFrenchWordsArray.slice(0);
+}
+
+let englishTranslationArray = JSON.parse(localStorage.getItem("englishWords"))
+if (englishTranslationArray === null) {
+    englishTranslationArray = defaultEnglishTranslationArray.slice(0);
+}
+
+actionButton.addEventListener('click', () => {
+    cardTitle.innerText = 'French Word:';
+    let wordArrayPosition = counter % frenchWordsArray.length;
+    currentWord.innerText = frenchWordsArray[wordArrayPosition];
+    document.getElementById('card-div').style.visibility = 'visible';
+    translateButton.disabled = false;
+    actionButton.innerText = 'Next';
+    if (actionButton.getAnimations().length === 0) {
+      actionButton.getAnimations()[0].cancel();
+    }
+    counter++;
+})
+
+translateButton.addEventListener('click', () => {
+    cardTitle.innerText = 'English Translation:';
+    let wordArrayPosition = frenchWordsArray.indexOf(currentWord.innerText);
+    currentWord.innerText = englishTranslationArray[wordArrayPosition];
+    translateButton.disabled = true;
+})
+
+addToList.addEventListener('click', () => {
+    if (frenchWordsArray.includes(newFrenchWord.value)) {
+      alert("Word already entered!")
+    } else if (newFrenchWord.value == "" || newEnglishWord.value == "") {
+        alert("Make sure both boxes are filled out!")
+    } else {
+      frenchWordsArray.push(newFrenchWord.value);
+      englishTranslationArray.push(newEnglishWord.value);
+      localStorage.setItem("frenchWords", JSON.stringify(frenchWordsArray))
+      localStorage.setItem("englishWords", JSON.stringify(englishTranslationArray))
+    }
+    newFrenchWord.value = "";
+    newEnglishWord.value = "";
+})
+
+resetList.addEventListener('click', () => {
+    console.log("Resetting list..")
+    frenchWordsArray = defaultFrenchWordsArray.slice(0);
+    console.log(frenchWordsArray);
+    englishTranslationArray = defaultEnglishTranslationArray.slice(0);
+    localStorage.clear();
+})
