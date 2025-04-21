bc79fc7b0b3ddc5c824f23917c3b6426088dd007
Asolvi-TManders <tim.manders@asolvi.com>
2025-04-21 20:39:42 +0100

    Add comments to script

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -13,6 +13,7 @@ const optionToggle = document.getElementById('option')
 let counter = 0;
 let currentPosition = 0;
 
+//Get translation direction and store locally if not set
 let storedOption = localStorage.getItem("option");
 switch (storedOption) {
   case null:
@@ -24,16 +25,19 @@ switch (storedOption) {
     break;
 }
 
+//Get foreign words from local storage and set to default list if empty
 let frenchWordsArray = JSON.parse(localStorage.getItem("frenchWords"))
 if (frenchWordsArray === null) {
     frenchWordsArray = defaultFrenchWordsArray.slice(0);
 }
 
+//Get english words from local storage and set to default list if empty
 let englishTranslationArray = JSON.parse(localStorage.getItem("englishWords"))
 if (englishTranslationArray === null) {
     englishTranslationArray = defaultEnglishTranslationArray.slice(0);
 }
 
+//Get Random integer for selecting position in words array - ensure different to previous
 function getRandomInteger(checkPosition) {
   let newPostion;
   do {
@@ -45,6 +49,7 @@ function getRandomInteger(checkPosition) {
 function takeAction () {
     
   let wordArrayPosition;
+  //If few words use simple option to reduce getting repeat words
   if (frenchWordsArray.length < 5) {
     wordArrayPosition = counter % frenchWordsArray.length;
   } else {
@@ -59,20 +64,23 @@ function takeAction () {
     currentWord.innerText = frenchWordsArray[wordArrayPosition];
   }
 
-  //document.getElementById('card-div').style.visibility = 'visible';
+  
   document.getElementById('card-div').style.display = 'block';
   translateButton.disabled = false;
   actionButton.innerText = 'Next';
+  //Cancel pulsing button after being pressed once
   if (actionButton.getAnimations().length !== 0) {
     actionButton.getAnimations()[0].cancel();
   }
   counter++;
+  //Set current position to pass into randomint function for checking it is different
   currentPosition = wordArrayPosition;
 }
 
 actionButton.addEventListener('click', takeAction);
 
 translateButton.addEventListener('click', () => {
+    //Change card title and switch to appropriate translated word
     let wordArrayPosition;
     if (storedOption === "englishToFrench"){
       cardTitle.innerText = 'French Translation:';
@@ -88,15 +96,19 @@ translateButton.addEventListener('click', () => {
 })
 
 addToList.addEventListener('click', () => {
+    //Check if word already in the array
     if (frenchWordsArray.includes(newFrenchWord.value)) {
       alert("Word already entered!")
+      //Check both boxes are populated
     } else if (newFrenchWord.value == "" || newEnglishWord.value == "") {
         alert("Make sure both boxes are filled out!")
     } else {
+      //If array only includes the default words empty the arrays
       if (frenchWordsArray.toString() === defaultFrenchWordsArray.toString()) {
         frenchWordsArray = [];
         englishTranslationArray = [];
       }
+      //Add the words to the arrays and set in local storage
       frenchWordsArray.push(newFrenchWord.value);
       englishTranslationArray.push(newEnglishWord.value);
       localStorage.setItem("frenchWords", JSON.stringify(frenchWordsArray))
@@ -107,6 +119,7 @@ addToList.addEventListener('click', () => {
 })
 
 resetList.addEventListener('click', () => {
+    //Set both arrays back to default values and clear local storage
     console.log("Resetting list..")
     frenchWordsArray = defaultFrenchWordsArray.slice(0);
     console.log(frenchWordsArray);
